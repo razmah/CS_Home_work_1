8ac6fd6d3b3ccaaefb39887f1d6eea245c097d2b
Nikita Razmahov <nikita.razmahov@yandex.ru>
2023-06-15 21:41:25 +0300

    add Task №8

---
+++ Task_8/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Напишите программу, которая на вход принимает число (N),
+а на выходе показывает все четные числа от 1 до N */
+
+Console.Clear();
+Console.Write("Введите число: ");
+
+string input = Console.ReadLine() ?? "";
+int N = int.Parse(input);
+int index = 1;
+
+while(index <= N)
+{
+    if (index % 2 == 0)
+    {
+        Console.Write($"{index} ");
+    }
+    index++;
+}
